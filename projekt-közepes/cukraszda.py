216f03f180a6e189bc5fc3d17d8a6d5a0475728a
BataVili <batavilmosl@elg-bp.edu.hu>
2026-02-05 18:14:59 +0100

    osztály + függvény + eljárás + txt beaddolása + lista elkészítése

---
+++ projekt-közepes/cukraszda.py
@@ -0,0 +1,15 @@
+import os,random
+os.system("cls")
+
+
+def Suti(nev:str,tipus:str,ar:int,egyseg:str):
+    eladas = 0
+    bevetel = 0
+
+def EladasGeneralas():
+    return random.randint(100, 500)
+
+def BevetelSzamitas():
+    bevetel = eladas * ar
+
+sutemenyek = []
